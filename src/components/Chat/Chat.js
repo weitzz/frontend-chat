aa6c16bdf0165e2979c534d95731f8021a664f39
Tatiane Weitzel <weitzeltati@Gmail.com>
2024-01-26 18:31:18 -0300

    deploy

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -14,7 +14,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://appchatsocketre.herokuapp.com/';
+  const ENDPOINT = process.env.ENDPOINT;
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
